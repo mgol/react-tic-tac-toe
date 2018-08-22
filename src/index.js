4e7b27648fa24593819cdcf358284692b4e44d93
Michał Gołębiowski-Owczarek <m.goleb@gmail.com>
2018-08-22 22:45:09 +0200

    A working game

--- src/index.js
+++ src/index.js
@@ -2,23 +2,75 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-class Square extends React.Component {
-    render() {
-        return (
-            <button className="square">
-                {/* TODO */}
-            </button>
-        );
+const calculateWinner = squares => {
+    const lines = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
+    ];
+
+    for (const line of lines) {
+        const [a, b, c] = line;
+        if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+            return squares[a];
+        }
     }
-}
+};
+
+const Square = props => (
+    <button
+        className="square"
+        onClick={props.onClick}>
+        {props.value}
+    </button>
+);
 
 class Board extends React.Component {
+    state = {
+        squares: Array(9).fill(null),
+        xIsNext: true,
+    };
+
+    handleClick(i) {
+        if (this.state.squares[i] || calculateWinner(this.state.squares)) {
+            return;
+        }
+
+        const squares = [...this.state.squares];
+        squares[i] = this.state.xIsNext ? 'X' : 'O';
+
+        this.setState(prevState => ({
+            squares,
+            xIsNext: !prevState.xIsNext,
+        }));
+    }
+
     renderSquare(i) {
-        return <Square />;
+        return (
+            <Square
+                value={this.state.squares[i]}
+                onClick={() => this.handleClick(i)}/>
+        );
     }
 
     render() {
-        const status = 'Next player: X';
+        const {squares, xIsNext} = this.state;
+
+        const winner = calculateWinner(squares);
+
+        let status;
+        if (winner) {
+            status = `Winner: ${ winner }`;
+        } else if (squares.every(square => square)) {
+            status = 'Draw';
+        } else {
+            status = `Next player: ${ xIsNext ? 'X' : 'O' }`;
+        }
 
         return (
             <div>
@@ -48,7 +100,7 @@ class Game extends React.Component {
         return (
             <div className="game">
                 <div className="game-board">
-                    <Board />
+                    <Board/>
                 </div>
                 <div className="game-info">
                     <div>{/* status */}</div>
@@ -62,6 +114,6 @@ class Game extends React.Component {
 // ========================================
 
 ReactDOM.render(
-    <Game />,
-    document.getElementById('root')
+    <Game/>,
+    document.getElementById('root'),
 );
